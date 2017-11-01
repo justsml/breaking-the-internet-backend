1fcba20dbc3c0ea995db22aae12e0b1a90da35c8
Miriam Quintana <quintana.miriam.cc@gmail.com>
2017-11-01 13:04:30 -0600

    created images model, ran migrations, registered image model to enable it, ran server to check that model was enabled using superuser login......and it worked.

--- src/breaking_the_internet_project/bti_api/models.py
+++ src/breaking_the_internet_project/bti_api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.utils import timezone
+default=timezone.now
 # Create your models here.
+
+class Images(models.Model):
+    """Stores urls of snapshots taken and the time they were created."""
+
+    image_url = models.URLField(default=timezone.now)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def image_store(self, url):
+        self.created_on = timezone.now()
+        self.image_url = url
+        self.save()
+
+        # return self.image_url
