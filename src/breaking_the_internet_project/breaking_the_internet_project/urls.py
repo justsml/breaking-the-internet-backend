e89d45d4b911ef8dbc35553a5552b8316c5fcff1
Miriam Quintana <quintana.miriam.cc@gmail.com>
2017-11-16 11:53:49 -0700

    added user login funtion

--- src/breaking_the_internet_project/breaking_the_internet_project/urls.py
+++ src/breaking_the_internet_project/breaking_the_internet_project/urls.py
@@ -18,4 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    AUTH_USER_MODEL = 'profiles_api.UserProfile'
 ]
--- src/breaking_the_internet_project/bti_api/admin.py
+++ src/breaking_the_internet_project/bti_api/admin.py
@@ -2,4 +2,6 @@ from django.contrib import admin
 from . import models
 
 # Register your models here.
+admin.site.register(models.UserProfile)
+admin.site.register(models.ProfileFeedItem)
 admin.site.register(models.Images)
